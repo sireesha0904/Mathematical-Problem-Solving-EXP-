64c3eb35d031a779a947ca23ea01b4c8da69e4a4
sireesha0904 <modisireesha09@gmail.com>
2025-02-17 16:12:30 +0530

    factorial

--- Exp-1/factorial.java
+++ Exp-1/factorial.java
@@ -9,6 +9,12 @@ public class Factorial {
         return n * factorialNum(n-1);
     }
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number to find factorial: ");
+        int n = sc.nextInt();
+        int fact = factorialNum(n);
+        System.out.println("Factorial of " + n + " is: " + fact);
+        sc.close();
 
     }
 }
