d2ffe1a2f5b0312e3e59a442f0c393b90d752227
sireesha0904 <modisireesha09@gmail.com>
2025-02-17 16:24:27 +0530

    factorial

--- Exp-1/factorial.java
+++ Exp-1/factorial.java
@@ -1,20 +1,20 @@
-package Exp1;
 import java.util.Scanner;
+
 public class Factorial {
-
-    public static int factorialNum(int n){
-        if(n==0 || n==1)
-        return 1;
-        else 
-        return n * factorialNum(n-1);
+    // Recursive method to find factorial
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
     }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number to find factorial: ");
-        int n = sc.nextInt();
-        int fact = factorialNum(n);
-        System.out.println("Factorial of " + n + " is: " + fact);
-        sc.close();
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        System.out.println("Factorial of " + num + " is: " + factorial(num));
+        sc.close();
+        ;
     }
 }
