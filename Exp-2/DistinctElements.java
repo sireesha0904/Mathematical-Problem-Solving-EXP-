b53877170571e5ae485b0ea50163c1b5226b7b59
sireesha0904 <modisireesha09@gmail.com>
2025-02-18 15:22:49 +0530

    DistinctElements

--- Exp-2/DistinctElements.java
+++ Exp-2/DistinctElements.java
@@ -1,3 +1,25 @@
+import java.util.*;
+
 public class DistinctElements {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt(); 
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt(); 
+        }
+
+        Set<Integer> distinctSet = new LinkedHashSet<>();  
+        for (int num : arr) {
+            distinctSet.add(num);
+        }
+
+        System.out.println("Distinct elements in the array: " + distinctSet);
+        sc.close();
+    }
 }
